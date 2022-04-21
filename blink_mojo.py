7478b65c30225632d32e854cdf69d25432670883
GERALD D NEALE <53587595+geraldneale@users.noreply.github.com>
2022-04-21 16:22:34 -0400

    Update blink_mojo.py

--- blink_mojo.py
+++ blink_mojo.py
@@ -169,10 +169,8 @@ def blink_mojo(faucet_coin, needs_privacy_coin,decoy_coin, decoy_value_coin):
     )
 
     #signature
-    # hex version of "hello", arbitrary message
-    DATA_TO_SIGN = msg
-    #genesis challenge
-    ADD_DATA= DEFAULT_CONSTANTS.AGG_SIG_ME_ADDITIONAL_DATA
+    DATA_TO_SIGN = msg #arbitrary message
+    ADD_DATA= DEFAULT_CONSTANTS.AGG_SIG_ME_ADDITIONAL_DATA #genesis challenge
     sig1 = AugSchemeMPL.sign(faucet_coin[1], DATA_TO_SIGN + bytes.fromhex(faucet_coin[0].get_hash().hex()) + ADD_DATA)
     sig2 = AugSchemeMPL.sign(needs_privacy_coin[1], DATA_TO_SIGN + bytes.fromhex(needs_privacy_coin[0].get_hash().hex()) + ADD_DATA)
     sig3 = AugSchemeMPL.sign(decoy_coin[1], DATA_TO_SIGN + bytes.fromhex(decoy_coin[0].get_hash().hex()) + ADD_DATA)
